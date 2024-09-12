eb3986cd1e92a21d1690d4e3951cfea78ab94bf9
Covllld <171270023+Covllld@users.noreply.github.com>
2024-09-12 05:45:57 -0700

    Update incrémentation.c

--- incrémentation.c
+++ incrémentation.c
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
 
 	int nombreinconnnu = 0;
-	nombreinconnnu = nombreinconnnu + 1;
+	nombreinconnnu = nombreinconnnu + 1; //ou nombreinconnu++; l'instruction ++ veut juste dire ajoute 1 a la valeur précédeente
 	printf("%d", nombreinconnnu);
 
 		return 0;
