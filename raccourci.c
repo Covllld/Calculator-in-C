cfb5afc7c3e63347c06eb64aa1e8bf66ffa9de03
Covllld <171270023+Covllld@users.noreply.github.com>
2024-09-12 05:53:09 -0700

    Create raccourci.c

---
+++ raccourci.c
@@ -0,0 +1,7 @@
+int nombre = 2;
+
+nombre += 4; // nombre vaut 6...
+nombre -= 3; // ... nombre vaut maintenant 3
+nombre *= 5; // ... nombre vaut 15
+nombre /= 3; // ... nombre vaut 5
+nombre %= 3; // ... nombre vaut 2 (car 5 = 1 * 3 + 2)
