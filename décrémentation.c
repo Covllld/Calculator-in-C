e33b34c78e4cc0fdabecfad8203f43a97ab7d9b1
Covllld <171270023+Covllld@users.noreply.github.com>
2024-09-12 05:50:13 -0700

    Create décrémentation.c

---
+++ décrémentation.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+
+	int nombreinconnnu = 0;
+	nombreinconnnu = nombreinconnnu - 1;// ou nombreinconnu--;
+	printf("%d", nombreinconnnu);
+
+		return 0;
+	}
+
+
+
