589f05051d03739c837ac385a689cf8938502fe6
Covllld <171270023+Covllld@users.noreply.github.com>
2024-09-12 05:35:41 -0700

    Create substraction.c

---
+++ substraction.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+
+		int resultat = 0, nbr1 = 0, nbr2 = 0;
+		printf("Donnez moi votre premiere valeur :");
+		scanf_s("%d", &nbr1);
+		printf("Donnez moi votre deuxieme valeur :");
+		scanf_s("%d", &nbr2);
+
+		resultat = nbr1 - nbr2;
+		printf(" %d - %d = %d", nbr1, nbr2, resultat);
+		return 0;
+	}
+
