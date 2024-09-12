b9b96ef06bd46083c99dc146fa00b862251d8613
Covllld <171270023+Covllld@users.noreply.github.com>
2024-09-12 05:44:05 -0700

    Create incrémentation.c

---
+++ incrémentation.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char* argv[]) {
+
+	int nombreinconnnu = 0;
+	nombreinconnnu = nombreinconnnu + 1;
+	printf("%d", nombreinconnnu);
+
+		return 0;
+	}
+
+
+
